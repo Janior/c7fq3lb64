2ea39aa7384bd6b75f92751a7af4bd17a1fc9e23
Janior <janior_orlando@hotmail.com>
2018-08-23 20:56:13 -0700

    Gastos con usuario y filtros solicitados

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,21 @@
 class ExpensesController < ApplicationController
-  def index
-     @expenses = Expense.order("date DESC")
-  end
+	before_action :authenticate_user!	
+
+	  def index
+	  	 user = User.find(current_user.id)
+	     @expenses = Expense.order("date DESC")
+        	# @expenses = Expense.order("date DESC")
+
+
+	        if (params[:concept].present?)
+	            @expenses =@expenses.where("concept like :query",query: "%#{params[:concept]}%")
+	        end
+	        if (params[:category_id].present?)
+	           @expenses= @expenses.where(category_id: params[:category_id])
+	        end
+
+	        if params[:concept].present?
+	           @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+	        end
+	  end
 end
